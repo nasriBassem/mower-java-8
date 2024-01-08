882287a0c96b847ecd8b079bb982de17007a45c9
nasriBassem <nasri_bassem@yahoo.fr>
2024-01-08 19:31:43 +0100

    noTicket | Mower :  add parser and formatter  | code

---
+++ src/main/java/org/mower/example/parser/MowerMapper.java
@@ -0,0 +1,10 @@
+package org.mower.example.parsser;
+
+import lombok.Data;
+
+@Data
+public class MowerMapper {
+        private String lawn ;
+        private String mower ;
+        private String instructions ;
+}
---
+++ src/main/java/org/mower/example/services/ParseInputFileService.java
@@ -0,0 +1,4 @@
+package org.mower.example.services;
+
+public class ParseInputFileService {
+}
